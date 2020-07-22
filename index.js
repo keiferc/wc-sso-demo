ee1a5fbe132bbfbbbe235181743eccd8004db469
Keifer <15503711+KeiferC@users.noreply.github.com>
2020-07-22 12:50:15 -0400

    set up sso

---
+++ index.js
@@ -0,0 +1,102 @@
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv")
+const express = require("express");
+const fs = require("fs");
+const passport = require("passport");
+const path = require("path");
+const saml = require("passport-saml");
+const session = require("express-session")
+
+const WELLESLEY_KEY = fs.readFileSync(__dirname + "/certs/wellesley_idp_key.pem", "utf8");
+const PVK = fs.readFileSync(__dirname + "/certs/key.pem", "utf8");
+const PUBLIC_KEY = fs.readFileSync(__dirname + "/certs/cert.pem", "utf8");
+const SSO_DEMO_PAGE = "sso-demo.html"
+
+const app = express();
+
+dotenv.config()
+
+const samlStrategy = new saml.Strategy(
+    {
+        callbackUrl: process.env.PUBLIC_HOST + "/login/callback",
+        entryPoint: process.env.ENTRY_POINT,
+        issuer: 'wc-ao-sso-demo',
+        identifierFormat: undefined,
+        cert: WELLESLEY_KEY,
+        decryptionPvk: PVK,
+        privateCert: PVK,
+        validateInResponseTo: false,
+        disableRequestedAuthnContext: true
+    }, 
+    function (profile, done) {
+        return done(null, profile);
+    }
+);
+
+passport.serializeUser(
+    function (user, done) {
+        console.log("Serializing user:");
+        console.log(user);
+        done(null, user);
+    }
+);
+
+passport.deserializeUser(
+    function (user, done) {
+        console.log('Deserializing user:');
+        console.log(user);
+        done(null, user);
+    }
+);
+
+passport.use('samlStrategy', samlStrategy);
+    
+// Express Instance Setup
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(
+    session(
+        {
+            secret: 'secret',
+            resave: false,
+            saveUninitialized: true
+        })
+);
+
+app.use(passport.initialize({}));
+
+
+// RESTful API
+app.get("/", 
+    function (req, res) {
+        res.sendFile(SSO_DEMO_PAGE, { root: path.join("src") });
+    }
+);
+
+app.get("/metadata", function (req, res) {
+    res.type("application/xml");
+    res.status(200).send(
+      samlStrategy.generateServiceProviderMetadata(PUBLIC_KEY, PUBLIC_KEY));
+});
+
+app.post('/login/callback',
+    function (req, res, next) {
+        console.log("Starting login callback...");
+        next();
+    },
+    passport.authenticate("samlStrategy"),
+    function (req, res) {
+        console.log("Log in user info:");
+        console.log(req.user);
+        res.send("Logged in successfully!");
+    }
+);
+
+
+// Server Setup
+const server = app.listen(3000, 
+    function () {
+        console.log('Listening on port %d', server.address().port);
+    }
+);
